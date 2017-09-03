055631615da730d130f7823d5856ad47c0768d88
jonicoma <johnfshaughnessy@gmail.com>
2017-09-03 15:56:37 +0800

    Create scene with scrolling foreground and background, and add buttons for menu and critterdex.

---
+++ Assets/Scripts/FloatLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatLeft : MonoBehaviour {
+
+    public Image One;
+    public Image Two;
+
+    public float Speed;
+
+    void Start()
+    {
+        One.rectTransform.localPosition = new Vector3(0, 0, 0);
+        Two.rectTransform.localPosition = new Vector3(2048, 0, 0);
+    }
+
+	void Update () {
+        One.rectTransform.localPosition += Vector3.left * Speed * Time.deltaTime;
+        if (One.rectTransform.localPosition.x <= -2048)
+        {
+            One.rectTransform.localPosition = new Vector3(2048, One.rectTransform.localPosition.y, One.rectTransform.localPosition.z);
+        }
+        Two.rectTransform.localPosition += Vector3.left * Speed * Time.deltaTime;
+        if (Two.rectTransform.localPosition.x <= -2048)
+        {
+            Two.rectTransform.localPosition = new Vector3(2048, Two.rectTransform.localPosition.y, Two.rectTransform.localPosition.z);
+        }
+	}
+}
